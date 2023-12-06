d22dd0a0cc38a9b40ae060aa657ef1778ea97d04
Xaivex <viethoj@gmail.com>
2023-12-06 17:50:24 -0600

    Updated firebase to store users personal data in Firestore database.

--- app/src/main/java/com/cs407/campuscrib/createAccount.java
+++ app/src/main/java/com/cs407/campuscrib/createAccount.java
@@ -11,14 +11,21 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class createAccount extends AppCompatActivity {
     FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
     }
+
     public void createAccountClick(View view) {
         EditText user = findViewById(R.id.createUsernameEditText);
         EditText pw = findViewById(R.id.createPasswordEditText);
@@ -39,10 +46,35 @@ public class createAccount extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                user.getText().clear();
-                                pw.getText().clear();
-                                goToActivity();
+                                // User creation successful
+                                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                                // Create a Map to represent user information
+                                Map<String, Object> userData = new HashMap<>();
+                                userData.put("email", username);
+                                userData.put("password", password);
+                                userData.put("repassword", repassword);
+
+                                // Store the user information in a Firestore collection named "users"
+                                db.collection("users")
+                                        .document(userId)
+                                        .set(userData)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    // Firestore update successful
+                                                    user.getText().clear();
+                                                    pw.getText().clear();
+                                                    goToActivity();
+                                                } else {
+                                                    // Handle the error
+                                                    Toast.makeText(createAccount.this, "Error storing user information", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
                             } else {
+                                // User creation failed
                                 Toast.makeText(createAccount.this, "Something went wrong. Try again!", Toast.LENGTH_SHORT).show();
                                 user.getText().clear();
                                 pw.getText().clear();
@@ -51,10 +83,12 @@ public class createAccount extends AppCompatActivity {
                     });
         }
     }
+
     public void onAlreadyGotLoginClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
     public void goToActivity() {
         Intent intent = new Intent(this, homePage.class);
         startActivity(intent);
