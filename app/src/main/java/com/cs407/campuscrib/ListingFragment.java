9fe7988825b326b534bb56fee8d20d37bf945cf3
cthao17 <rororm17@gmail.com>
2023-12-10 15:27:12 -0600

    Delete images when delete button is clicked

--- app/src/main/java/com/cs407/campuscrib/ListingFragment.java
+++ app/src/main/java/com/cs407/campuscrib/ListingFragment.java
@@ -2,11 +2,14 @@ package com.cs407.campuscrib;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,12 +21,16 @@ import com.cs407.campuscrib.model.Chatroom;
 import com.cs407.campuscrib.model.ListingModel;
 import com.cs407.campuscrib.utils.FirebaseUtil;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.Firebase;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +45,10 @@ public class ListingFragment extends Fragment implements YourListingAdapter.OnEd
         recyclerView = rootView.findViewById(R.id.recycler_view);
         setUpListingView();
         return rootView;
+    }
+
+    interface OnFolderDeleteListener {
+        void onFolderDeleteFailure(Exception exception);
     }
 
     void setUpListingView() {
@@ -101,9 +112,9 @@ public class ListingFragment extends Fragment implements YourListingAdapter.OnEd
     @Override
     public void onDeleteClick(String listingId) {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String uid = user.getUid();
+        String uid = user.getUid();
 
+        if (user != null) {
             FirebaseFirestore.getInstance()
                     .collection("users")
                     .document(uid)
@@ -113,11 +124,45 @@ public class ListingFragment extends Fragment implements YourListingAdapter.OnEd
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             setUpListingView();
-                            Toast.makeText(getContext(), "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                            deleteImageFolder(uid, listingId);
+                            Toast.makeText(getContext(), "Successfully Deleted Listing", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(getContext(), "Error Deleting Listing", Toast.LENGTH_SHORT).show();
                         }
                     });
         }
+    }
+
+    // Retrieve image URIs and delete corresponding files
+    private void getImageURIsAndDelete(final StorageReference folderRef, final OnFolderDeleteListener listener) {
+        folderRef.listAll().addOnSuccessListener(listResult -> {
+            // Retrieve image URIs
+            List<String> imageURIs = new ArrayList<>();
+            for (StorageReference item : listResult.getItems()) {
+                imageURIs.add(item.toString()); // You might need to adjust this based on how URIs are stored
+            }
+
+            // Delete each file and corresponding URI
+            for (int i = 0; i < listResult.getItems().size(); i++) {
+                StorageReference itemRef = listResult.getItems().get(i);
+                String uriToDelete = imageURIs.get(i);
+
+                // Delete the file
+                itemRef.delete().addOnFailureListener(exception -> {
+                    // Handle failure to delete individual file
+                    listener.onFolderDeleteFailure(exception);
+                });
+            }
+        });
+    }
+
+    private void deleteImageFolder(String uid, String listingId) {
+        StorageReference folderRef = FirebaseUtil.getPersonalListingImageRef().child(listingId);
+         getImageURIsAndDelete(folderRef, new OnFolderDeleteListener() {
+             @Override
+             public void onFolderDeleteFailure(Exception exception) {
+                 Toast.makeText(getContext(), "Error Deleting Images", Toast.LENGTH_SHORT).show();
+             }
+         });
     }
 }
